c654c458a866635bee645fd905efc8de3e3d746e
Muhammad Redyka Fazry <fazry@theravenry.com>
2020-10-05 21:09:18 +0700

    Fix home page, add images

--- src/pages/JoinWorkshop.js
+++ src/pages/JoinWorkshop.js
@@ -3,35 +3,42 @@ import { Link } from "react-router-dom";
 
 export default function JoinWorkshop() {
   return (
-    <>
-      <div>
-        <h4>Friday, 11 Desember 2020</h4>
-        <Link to="/joinWorkshop/workshop1">
-          <div>
-            <span>Workshop 1 : Workshop of ACS</span><br/>
-            <span>14.00 - 16.30</span>
-          </div> 
-        </Link>
-        <Link to="/joinWorkshop/workshop2">
-          <div>
-            <span>Workshop II : Workshop of Echocardiography in congenital heart disease (Pokja Pediatric Cardiology PERKI)</span><br/>
-            <span>14.00 - 16.30</span>
-          </div> 
-        </Link>
-        <h4>Friday, 18 Desember 2020</h4>
-        <Link to="/joinWorkshop/workshop3">
-          <div>
-            <span>Workshop III : Workshop of Arrhythmia in Clinical Practice</span><br/>
-            <span>14.00 - 16.30</span>
-          </div> 
-        </Link>
-        <Link to="/joinWorkshop/workshop4">
-          <div>
-            <span>Workshop IV : Workshop of Acute Heart Failure</span><br/>
-            <span>14.00 - 16.30</span>
-          </div> 
-        </Link>
-      </div>
-    </>
+    <div className="page">
+      <h4>Friday, 11 Desember 2020</h4>
+      <Link to="/joinWorkshop/workshop1">
+        <div>
+          <span>Workshop 1 : Workshop of ACS</span>
+          <br />
+          <span>14.00 - 16.30</span>
+        </div>
+      </Link>
+      <Link to="/joinWorkshop/workshop2">
+        <div>
+          <span>
+            Workshop II : Workshop of Echocardiography in congenital heart
+            disease (Pokja Pediatric Cardiology PERKI)
+          </span>
+          <br />
+          <span>14.00 - 16.30</span>
+        </div>
+      </Link>
+      <h4>Friday, 18 Desember 2020</h4>
+      <Link to="/joinWorkshop/workshop3">
+        <div>
+          <span>
+            Workshop III : Workshop of Arrhythmia in Clinical Practice
+          </span>
+          <br />
+          <span>14.00 - 16.30</span>
+        </div>
+      </Link>
+      <Link to="/joinWorkshop/workshop4">
+        <div>
+          <span>Workshop IV : Workshop of Acute Heart Failure</span>
+          <br />
+          <span>14.00 - 16.30</span>
+        </div>
+      </Link>
+    </div>
   );
 }
