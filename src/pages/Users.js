31c9a2a9dfcfda1de4fef7789a01501ac4f2f193
Muhammad Redyka Fazry <fazry@theravenry.com>
2020-10-17 23:09:22 +0700

    Add update user loading in admin dashboard

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -8,6 +8,7 @@ export default function Users() {
   const [filter, setFilter] = useState("");
   const [sort, setSort] = useState({ col: 0, mode: "desc" });
   const [loading, setLoading] = useState(false);
+  const [updating, setUpdating] = useState(false);
 
   function getUsers() {
     setLoading(true);
@@ -68,99 +69,96 @@ export default function Users() {
   return (
     <div className="page">
       <Header as="h1">List of Registered Participants</Header>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          <Input
-            icon="search"
-            placeholder="Search by name, email, or phone number"
-            onChange={(e) => setFilter(e.target.value)}
-          />
-          <Table>
-            <Table.Header>
-              <Table.Row>
-                {columns.map((col, index) => (
-                  <Table.HeaderCell
-                    key={`col-${index}-${col}`}
-                    style={{ cursor: "pointer" }}
-                    onClick={() => {
-                      const mode =
-                        sort.col === index && sort.mode === "asc"
-                          ? "desc"
-                          : "asc";
-                      setSort({ col: index, mode: mode });
-                    }}
-                  >
-                    {col}{" "}
-                    <Icon
-                      name={
-                        index !== sort.col
-                          ? "sort"
-                          : sort.mode === "asc"
-                          ? "sort up"
-                          : "sort down"
-                      }
-                    />
-                  </Table.HeaderCell>
-                ))}
-              </Table.Row>
-            </Table.Header>
 
-            <Table.Body>
-              {users
-                .filter(
-                  (user) =>
-                    (user.Email.includes(filter) ||
-                      user.FirstName.includes(filter) ||
-                      user.LastName.includes(filter) ||
-                      user.PhoneNumber.includes(filter)) &&
-                    user.Email !== "bacuponline2020@gmail.com"
-                )
-                .map((user) => (
-                  <Table.Row key={`user-${user.Email}`}>
-                    <Table.Cell>
-                      {user.FirstName} {user.LastName}
-                    </Table.Cell>
-                    <Table.Cell>{user.Email}</Table.Cell>
-                    <Table.Cell>{user.PhoneNumber}</Table.Cell>
-                    <Table.Cell>{user.Package}</Table.Cell>
-                    <Table.Cell>{user.Category}</Table.Cell>
-                    <Table.Cell>IDR {user.Price.toLocaleString()}</Table.Cell>
-                    <Table.Cell>
-                      {user.Status === 1 && (
-                        <Button
-                          onClick={() => {
-                            axios
-                              .patch(
-                                `http://backend.bacup.co/v/1/user?UserId=${user.UserID}`,
-                                { Status: 2 },
-                                {
-                                  headers: {
-                                    Authorization: localStorage.getItem(
-                                      "TOKEN"
-                                    ),
-                                  },
-                                }
-                              )
-                              .then((res) => {
-                                getUsers();
-                              })
-                              .catch((err) => {
-                                console.log(err);
-                              });
-                          }}
-                        >
-                          Send password
-                        </Button>
-                      )}
-                    </Table.Cell>
-                  </Table.Row>
-                ))}
-            </Table.Body>
-          </Table>
-        </>
-      )}
+      {(updating || loading) && <Loader floating></Loader>}
+      <Input
+        icon="search"
+        placeholder="Search by name, email, or phone number"
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            {columns.map((col, index) => (
+              <Table.HeaderCell
+                key={`col-${index}-${col}`}
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  const mode =
+                    sort.col === index && sort.mode === "asc" ? "desc" : "asc";
+                  setSort({ col: index, mode: mode });
+                }}
+              >
+                {col}{" "}
+                <Icon
+                  name={
+                    index !== sort.col
+                      ? "sort"
+                      : sort.mode === "asc"
+                      ? "sort up"
+                      : "sort down"
+                  }
+                />
+              </Table.HeaderCell>
+            ))}
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {users
+            .filter(
+              (user) =>
+                (user.Email.includes(filter) ||
+                  user.FirstName.includes(filter) ||
+                  user.LastName.includes(filter) ||
+                  user.PhoneNumber.includes(filter)) &&
+                user.Email !== "bacuponline2020@gmail.com"
+            )
+            .map((user) => (
+              <Table.Row key={`user-${user.Email}`}>
+                <Table.Cell>
+                  {user.FirstName} {user.LastName}
+                </Table.Cell>
+                <Table.Cell>{user.Email}</Table.Cell>
+                <Table.Cell>{user.PhoneNumber}</Table.Cell>
+                <Table.Cell>{user.Package}</Table.Cell>
+                <Table.Cell>{user.Category}</Table.Cell>
+                <Table.Cell>IDR {user.Price.toLocaleString()}</Table.Cell>
+                <Table.Cell>
+                  {user.Status === 1 && (
+                    <Button
+                      onClick={() => {
+                        setUpdating(true);
+                        axios
+                          .patch(
+                            `http://backend.bacup.co/v/1/user?UserId=${user.UserID}`,
+                            { Status: 2 },
+                            {
+                              headers: {
+                                Authorization: localStorage.getItem("TOKEN"),
+                              },
+                            }
+                          )
+                          .then((res) => {
+                            getUsers();
+                          })
+                          .catch((err) => {
+                            console.log(err);
+                          })
+                          .finally(() => {
+                            setUpdating(false);
+                          });
+                      }}
+                      disabled={updating}
+                    >
+                      Send password
+                    </Button>
+                  )}
+                </Table.Cell>
+              </Table.Row>
+            ))}
+        </Table.Body>
+      </Table>
     </div>
   );
 }
